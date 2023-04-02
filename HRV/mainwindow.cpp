30479c0eade089e251022ad5571c14aaafcccf85
MountainTorque <56087980+MountainTorque@users.noreply.github.com>
2023-04-01 23:30:25 -0400

    Added battery

--- HRV/mainwindow.cpp
+++ HRV/mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Initialize timer counter
+    currentTimerCount = -1;
+
     // Create menu tree
     masterMenu = new Menu("MAIN MENU", {"START A NEW SESSION","SETTINGS","HISTORY"}, nullptr);
     mainMenuOG = masterMenu;
@@ -25,12 +29,25 @@ MainWindow::MainWindow(QWidget *parent)
 
     //git test
 
+    // charge button connections
+    connect(ui->chargeAdminButton, &QPushButton::released, this, &MainWindow::rechargeBattery);
+
+    // Battery level spin box connections
+    connect(ui->batteryLevelAdminSpinBox, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &MainWindow::changeBatteryLevel);
+
     // device interface button connections
     connect(ui->upButton, &QPushButton::pressed, this, &MainWindow::navigateUpMenu);
     connect(ui->downButton, &QPushButton::pressed, this, &MainWindow::navigateDownMenu);
     connect(ui->okButton, &QPushButton::pressed, this, &MainWindow::navigateSubMenu);
     connect(ui->backButton, &QPushButton::pressed, this, &MainWindow::navigateBack);
     connect(ui->menuButton, &QPushButton::pressed, this, &MainWindow::navigateToMainMenu);
+
+    // Initialize battery levels
+    ui->batteryLevelAdminSpinBox->setValue(batteryLvl);
+    currentTimerCount = 0;
+    // initialize the timer
+    timer = new QTimer(this);
+    initializeTimer(timer);
 }
 
 MainWindow::~MainWindow()
@@ -60,6 +77,14 @@ void MainWindow::initializeMainMenu(Menu* m) {
     Menu* clearHistory = new Menu("CLEAR", {"YES","NO"}, history);
     history->addChildMenu(viewHistory);
     history->addChildMenu(clearHistory);
+}
+
+void MainWindow::initializeTimer(QTimer* t) {
+
+    connect(t, &QTimer::timeout, this, &MainWindow::updateTimer);
+
+    t->start(1000);
+
 }
 
 void MainWindow::navigateUpMenu() {
@@ -215,4 +240,53 @@ void MainWindow::changePowerStatus() {
 void MainWindow::powerChange() {
     powerStatus  = !powerStatus;
     changePowerStatus();
+}
+
+void MainWindow::rechargeBattery() {
+
+    int batteryLevel = 100.0;
+    changeBatteryLevel(batteryLevel);
+}
+
+void MainWindow::updateTimer() {
+
+    drainBattery();
+}
+
+void MainWindow::changeBatteryLevel(double newLevel) {
+
+    if (newLevel >= 0.0 && newLevel <= 100.0) {
+        if (newLevel == 0.0 && powerStatus == true) {
+            powerChange();
+            batteryLvl = 0;
+        }else{
+            batteryLvl = newLevel;
+        }
+
+        ui->batteryLevelAdminSpinBox->setValue(newLevel);
+        int newLevelInt = int(newLevel);
+        ui->batteryLevelBar->setValue(newLevelInt);
+
+        QString highBatteryHealth = "QProgressBar { selection-background-color: rgb(78, 154, 6); background-color: rgb(0, 0, 0); }";
+        QString mediumBatteryHealth = "QProgressBar { selection-background-color: rgb(196, 160, 0); background-color: rgb(0, 0, 0); }";
+        QString lowBatteryHealth = "QProgressBar { selection-background-color: rgb(164, 0, 0); background-color: rgb(0, 0, 0); }";
+
+        if (newLevelInt >= 50) {
+            ui->batteryLevelBar->setStyleSheet(highBatteryHealth);
+        }
+        else if (newLevelInt >= 20) {
+            ui->batteryLevelBar->setStyleSheet(mediumBatteryHealth);
+        }
+        else {
+            ui->batteryLevelBar->setStyleSheet(lowBatteryHealth);
+        }
+    }
+}
+
+void MainWindow::drainBattery() {
+
+    //1000 constant because 15 minutes is the longest therapy and we feel as it should last at least 15 minutes at full power
+    double batteryLevel = batteryLvl - 0.05;
+
+    changeBatteryLevel(batteryLevel);
 }
